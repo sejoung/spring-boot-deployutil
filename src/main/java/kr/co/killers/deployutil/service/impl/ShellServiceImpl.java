0a8a723b2b2720bc708a1ce66334e2a56a946d33
sejoung <sejoung@gmail.com>
2015-11-11 17:22:04 +0900

    common-io joda-time 추가
    common-io joda-time 추가

--- src/main/java/kr/co/killers/deployutil/service/impl/ShellServiceImpl.java
+++ src/main/java/kr/co/killers/deployutil/service/impl/ShellServiceImpl.java
@@ -1,22 +1,25 @@
 package kr.co.killers.deployutil.service.impl;
 
+import java.io.File;
 import java.util.Map;
 
+import org.h2.store.fs.FileUtils;
+import org.joda.time.DateTime;
 import org.springframework.stereotype.Service;
 
 import kr.co.killers.deployutil.service.ShellService;
-
 
 @Service
 public class ShellServiceImpl implements ShellService {
 
 	@Override
 	public void MakeShell(Map<String, String> makefileList) {
+		DateTime dt = new DateTime();
 
 		
-		
-		
+		String dir = "C://temp" + File.separatorChar + "";
+		FileUtils.createDirectories(dir);
+
 	}
 
-	
 }
